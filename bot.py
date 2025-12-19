1aa459968059fb03f3d3cb097e2fad8cb85e6c18
emanueltavolini <emanueltavolini@gmail.com>
2025-12-19 16:50:55 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,8 +11,9 @@ async def comenzar(update: Update, contexto: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bienvenid@ al mejor servidor de Minecraft que podras encontrar, usa /ip (para poder ver la IP del servidor), tambien puedes usar /discord para ir a nuestro Discord oficial")
 async def link_de_discord(update: Update, contexto: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Unete a nuestro Discord: https://discord.gg/WSd9WT7fme")
-aplicacion = ApplicationBuilder().token(token).build()
+aplicacion = ApplicationBuilder().token("8285635468:AAExfzIHqLDy-YiS-fDmjstX_guPQvkZ-cU").build()
 aplicacion.add_handler(CommandHandler("start", comenzar))
 aplicacion.add_handler(CommandHandler("ip", decir_ip))
 aplicacion.add_handler(CommandHandler("discord", link_de_discord))
 aplicacion.run_polling(allowed_updates=Update.ALL_TYPES)
+
